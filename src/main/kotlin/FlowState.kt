7e550d41a28554adb72232efa01658fc8ca64bbd
Mark Henke <gilligan128@gmail.com>
2019-05-25 09:43:11 -0400

    Revert "trying out different design for fun"
    This reverts commit c5b7e82b81c34e4abf2fa247c8baf09a9e9a5102.

--- src/main/kotlin/FlowState.kt
+++ src/main/kotlin/FlowState.kt
@@ -4,5 +4,4 @@ sealed class FlowState {
     data class DishwasherRunning(val dishesOnCounter: Int, val dishesInWasher: Int, val currentMeal: Meal): FlowState()
     data class DishwasherFinished(val dishesOnCounter: Int, val currentMeal: Meal) : FlowState()
     data class MealTime(val dishesOnCounter: Int, val dishesInWasher: Int, val currentMeal: Meal): FlowState()
-    object Stopped: FlowState()
 }
