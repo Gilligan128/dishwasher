8a5939c400e88e22673a963611fbaabe91b15404
Mark Henke <gilligan128@gmail.com>
2019-05-25 11:40:00 -0400

    starting round 2 of state machine refactoring

--- src/main/kotlin/DishwasherFlowState.kt
+++ src/main/kotlin/DishwasherFlowState.kt
@@ -2,6 +2,12 @@ data class DishwasherFlowState(
     val numberOfDishesOnCounter: Int = 0,
     val numberOfDishesInWasher: Int = 0,
     val dishwasherRunning: Boolean = false,
-    val currentMeal: Meal = Meal.Breakfast
+    val currentMeal: Meal = Meal.Breakfast,
+    val dishwasherState: DishwasherState2 = DishwasherState2.Idle()
 )
 
+sealed class DishwasherState2(open val meal:Meal = Meal.Breakfast) {
+    data class Running(val dishesOnCounter: Int, val dishesInWasher: Int, val hoursLeftToRun: Double, override val meal: Meal) : DishwasherState2(meal)
+    data class Idle(val dishesInWasher: Int = 0, override val meal: Meal = Meal.Breakfast) : DishwasherState2(meal)
+    data class Finished(val dishesOnCounter: Int, override val meal: Meal): DishwasherState2(meal)
+}
